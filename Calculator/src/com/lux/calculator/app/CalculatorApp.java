7bd39d9f1752e03bb57a46b2d7f02c7eccb6b4ac
Pavel <Pavel@Pavel-PC>
2019-09-08 14:38:07 +0300

    434

--- Calculator/src/com/lux/calculator/app/CalculatorApp.java
+++ Calculator/src/com/lux/calculator/app/CalculatorApp.java
@@ -4,7 +4,7 @@ import com.lux.calculator.ui.CalculatorView;
 
 public class CalculatorApp {
 
-	public static void main(String[] args) {//
+	public static void main(String[] args) {
 		CalculatorView calculatorview = new CalculatorView();
 		calculatorview.open();
 	}
